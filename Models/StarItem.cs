1d1a0c316b9dd35ef7ed0664cf842b752ef31294
= <=>
2025-11-02 23:30:22 +0100

    name +

--- Models/StarItem.cs
+++ Models/StarItem.cs
@@ -29,7 +29,7 @@ public class StarItem
         Random random = new Random();
 
         int randId = 0;
-        string[] nameList = ["Picoball", "Fishtank", "Stefan", "P8-AR", "P4-AR"];
+        string[] nameList = ["Picoball", "Fishtank", "Stefan", "P8-AR", "P4-AR", "Hydroponic Farm", "Quantum Drive", "Cargo Rack", "MedPen", "Nano-Forge", "Shield Generator", "Fuel Scoop", "Afterburner", "Auto-Loader", "Cooler", "Power Plant", "Thruster", "Warp Core", "Quantum Core", "Hull Reinforcement", "Armor Plating", "EMP Launcher", "Missile Rack", "Laser Cannon", "Railgun", "Gauss Cannon", "Plasma Accelerator", "Point Defense", "Mining Laser", "Salvage Arm", "Tractor Beam", "Refinery", "Jump Drive"];
         string randName = random.GetItems<string>(nameList, 1)[0];
         int randomLocation = 67;
         int randQuantity = (int)(random.NextSingle() * 100.0);
