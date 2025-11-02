d58ef6a00970646cfab830c9f8a831f39c098b80
= <=>
2025-11-02 23:28:42 +0100

    random items går til runkebua

--- Models/StarItem.cs
+++ Models/StarItem.cs
@@ -31,7 +31,7 @@ public class StarItem
         int randId = 0;
         string[] nameList = ["Picoball", "Fishtank", "Stefan", "P8-AR", "P4-AR"];
         string randName = random.GetItems<string>(nameList, 1)[0];
-        int randomLocation = 0;
+        int randomLocation = 67;
         int randQuantity = (int)(random.NextSingle() * 100.0);
         bool randIsShared = random.GetItems<bool>([true, false], 1)[0];
 
