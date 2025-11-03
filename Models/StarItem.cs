c7578bf18a75692e61b365689b0f79d19dd7a280
Even <even.andreas.ingeberg@ebnett.no>
2025-11-03 15:53:30 +0100

    random item works with location

--- Models/StarItem.cs
+++ Models/StarItem.cs
@@ -28,10 +28,12 @@ public class StarItem
     {
         Random random = new Random();
        
+        db.StarLocations.Count();
+
         int randId = 0;
         string[] nameList = ["Picoball", "Fishtank", "Stefan", "P8-AR", "P4-AR", "Hydroponic Farm", "Quantum Drive", "Cargo Rack", "MedPen", "Nano-Forge", "Shield Generator", "Fuel Scoop", "Afterburner", "Auto-Loader", "Cooler", "Power Plant", "Thruster", "Warp Core", "Quantum Core", "Hull Reinforcement", "Armor Plating", "EMP Launcher", "Missile Rack", "Laser Cannon", "Railgun", "Gauss Cannon", "Plasma Accelerator", "Point Defense", "Mining Laser", "Salvage Arm", "Tractor Beam", "Refinery", "Jump Drive"];
         string randName = random.GetItems<string>(nameList, 1)[0];
-        int locationId = 0;
+        int locationId = random.Next(0, db.StarLocations.Count()-1);
         int randQuantity = (int)(random.NextSingle() * 100.0);
         bool randIsShared = random.GetItems<bool>([true, false], 1)[0];
 
