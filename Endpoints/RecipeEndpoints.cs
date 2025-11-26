cc6ed51715b18ee56466ffe50ef62ddf7d21a1ae
Even <even.andreas.ingeberg@ebnett.no>
2025-11-26 09:43:35 +0100

    fixed auth requirements for recipes

--- Endpoints/RecipeEndpoints.cs
+++ Endpoints/RecipeEndpoints.cs
@@ -19,7 +19,7 @@ public static class RecipeEndpoints
 
             return TypedResults.Created();
             
-        });
+        }).RequireAuthorization("org");
 
         recipeApi.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (ItemCacheDb db, int id) =>
         {
@@ -27,7 +27,7 @@ public static class RecipeEndpoints
             if (!result) return TypedResults.NotFound();
 
             return TypedResults.Ok();
-        });
+        }).RequireAuthorization("org");
 
         recipeApi.MapPut("/{id}", async Task<Results<Ok, NotFound>> (ItemCacheDb db, Recipe recipe, int id) =>
         {
@@ -35,7 +35,7 @@ public static class RecipeEndpoints
             if (!result) return TypedResults.NotFound();
 
             return TypedResults.Ok();
-        });
+        }).RequireAuthorization("org");
     }
 
 }
