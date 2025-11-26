233166428eaf5822543cccbd43d1e77b56072ebf
Even <even.andreas.ingeberg@ebnett.no>
2025-11-26 08:30:54 +0100

    update updatecache

--- Endpoints/DevEndpoints.cs
+++ Endpoints/DevEndpoints.cs
@@ -9,48 +9,60 @@ public static class DevEndpoints
             .RequireAuthorization("dev");
 
         // Update the Cache from UEX and compile a list of locations
-        devApi.MapGet("/updateCache", async Task<Results<Ok<CacheUpdateResponse>, InternalServerError>> (ItemCacheDb db, IHttpClientFactory httpClientFactory) =>
+        devApi.MapGet("/updateCache", async Task<Results<Ok<CacheUpdateResponse>, InternalServerError>> (bool updateItems, bool updateLocations, ItemCacheDb db, IHttpClientFactory httpClientFactory) =>
         {
             var client = httpClientFactory.CreateClient("UexApi");
 
-            bool catResult = await db.UpdateCategories(db, client);
-            if (!catResult)
+
+
+            if (updateItems)
             {
-                Console.WriteLine("Failed to update categories");
-                return TypedResults.InternalServerError();
-            }
-            bool poiResult = await db.UpdatePois(db, client);
-            if (!poiResult)
-            {
-                Console.WriteLine("Failed to update pois");
-                return TypedResults.InternalServerError();
-            }
-            bool spaceStationResult = await db.UpdateSpaceStations(db, client);
-            if (!spaceStationResult)
-            {
-                Console.WriteLine("Failed to update space stations");
-                return TypedResults.InternalServerError();
-            }
-            bool cityResult = await db.UpdateCities(db, client);
-            if (!cityResult)
-            {
-                Console.WriteLine("Failed to update cities");
-                return TypedResults.InternalServerError();
+                bool catResult = await db.UpdateCategories(db, client);
+                if (!catResult)
+                {
+                    Console.WriteLine("Failed to update categories");
+                    return TypedResults.InternalServerError();
+                }
+                bool itemResult = await db.UpdateItems(db, client);
+                
+                if (!itemResult)
+                {
+                    Console.WriteLine("Failed to update items");
+                    return TypedResults.InternalServerError();
+                }
             }
             
-            bool locationMergeResult = await db.CompileLocations(db);
-            if (!locationMergeResult)
+           if (updateLocations)
             {
-                Console.WriteLine("Failed to compile locations");
-                return TypedResults.InternalServerError();
+                 bool poiResult = await db.UpdatePois(db, client);
+                if (!poiResult)
+                {
+                    Console.WriteLine("Failed to update pois");
+                    return TypedResults.InternalServerError();
+                }
+                bool spaceStationResult = await db.UpdateSpaceStations(db, client);
+                if (!spaceStationResult)
+                {
+                    Console.WriteLine("Failed to update space stations");
+                    return TypedResults.InternalServerError();
+                }
+                bool cityResult = await db.UpdateCities(db, client);
+                if (!cityResult)
+                {
+                    Console.WriteLine("Failed to update cities");
+                    return TypedResults.InternalServerError();
+                }
+                
+                bool locationMergeResult = await db.CompileLocations(db);
+                if (!locationMergeResult)
+                {
+                    Console.WriteLine("Failed to compile locations");
+                    return TypedResults.InternalServerError();
+                }
             }
 
-            bool itemResult = await db.UpdateItems(db, client);
-            if (!itemResult)
-            {
-                Console.WriteLine("Failed to update items");
-                return TypedResults.InternalServerError();
-            }
+            
+
             return TypedResults.Ok(await db.GetCacheUpdateResponse(db));
         });
 
